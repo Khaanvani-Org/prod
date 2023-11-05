6869b4515c821dfbaa74cb3294ed21e2053ef4d8
vidushi <eng21ct@dsu.edu.in>
2023-11-05 13:38:49 +0530

    modified:   modules/eks/main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -46,8 +46,8 @@ resource "aws_security_group" "node_group_one" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port = 80
-    to_port   = 80
+    from_port = 8501
+    to_port   = 8501
     protocol  = "tcp"
 
     cidr_blocks = ["0.0.0.0/0"]
