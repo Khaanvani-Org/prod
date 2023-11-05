8cad864cf2945bbbec83e24ac297b1335b662596
s-sukriti <eng21ct0039@dsu.edu.in>
2023-11-05 10:57:25 +0000

    Update from https://github.com/Khaanvani-Org/test/commit/0fb05bfbe4ec09841ae00806c5f8d42b90f39fe3

--- pytest.py
+++ pytest.py
@@ -16,6 +16,68 @@ def test_non_greeting(chatbot_fixture):
     response = chatbot_instance.respond("How's the weather?")
     assert response == "I don't understand."
 
+
+def test_empty_input(chatbot_fixture):
+    chatbot_instance = chatbot_fixture
+    response = chatbot_instance.respond("")
+    assert response == "Please provide a valid input."
+    
+def test_basic(chatbot_fixture):
+    chatbot_instance = chatbot_fixture
+    response = chatbot_instance.respond("What steps should I take if I have been injured in a mine site?")
+    assert response == "Ensure Immediate Safety: Prioritize your safety and the safety of others. If you are injured, seek medical attention immediately. If it is safe to do so, move to a secure location within the mine."
+
+def test_question(chatbot_fixture):
+    chatbot_instance = chatbot_fixture
+    response = chatbot_instance.respond("Who should I contact for help and assistance?")
+    assert response == " If you have been injured in a mine due to inadequate safety measures, you should consider seeking help and assistance from the following: Workers' Union: Contact your workers' union, if applicable, as they can provide guidance and support in dealing with the situation. Support Groups: Reach out to support groups or organizations that specialize in mine safety and workers' rights. They can offer advice and assistance in navigating the process of claiming compensation. Attorney: Consult with an attorney who specializes in labor and mining laws. They can provide legal advice, assess your case, and guide you through the process of claiming compensation from the government or authorities. It is important to seek professional advice and support to ensure that your rights are protected and that you follow the appropriate steps for your specific situation."
+
+def test_goodbye(chatbot_fixture):
+    chatbot_instance = chatbot_fixture
+    response = chatbot_instance.respond("What medical treatment is available for mining accident injuries?")
+    assert response == "Initial stabilization: This may involve procedures to stop bleeding, immobilize fractures, or address any life-threatening conditions.Surgical intervention: If necessary, surgery may be performed to repair fractures, treat internal injuries, or address other significant injuries.Wound care: Proper cleaning, dressing, and care of wounds are essential to prevent infection and promote healing.Pain management: Medications or other interventions may be provided to manage pain and discomfort associated with the injury."
+
+def test_long_query(chatbot_fixture):
+    chatbot_instance = chatbot_fixture
+    response = chatbot_instance.respond("Can I file a workers' compensation claim if I'm injured in a mining accident?")
+    assert response == "Yes, you can file a workers' compensation claim if you are injured in a mining accident. Workers' compensation is a type of insurance that provides benefits to employees who suffer work-related injuries or illnesses. It is designed to cover medical expenses, lost wages, and rehabilitation costs for injured workers."
+
+def test_emoticons(chatbot_fixture):
+    chatbot_instance = chatbot_fixture
+    response = chatbot_instance.respond("I'm feeling happy today!")
+    assert response == "I'm glad to hear that!"
+
+def test_insult(chatbot_fixture):
+    chatbot_instance = chatbot_fixture
+    response = chatbot_instance.respond("You're a terrible chatbot!")
+    assert response == "I'm here to help. If you have any concerns, please let me know."
+
+def test_custom_commands(chatbot_fixture):
+    chatbot_instance = chatbot_fixture
+    response = chatbot_instance.respond("/help")
+    assert response == "Available commands: /start, /stop, /help"
+
+def test_multiline_input(chatbot_fixture):
+    chatbot_instance = chatbot_fixture
+    response = chatbot_instance.respond("Can you help me with the following? filling of the form ")
+    assert "I can assist with your tasks" in response
+
+def test_special_characters(chatbot_fixture):
+    chatbot_instance = chatbot_fixture
+    response = chatbot_instance.respond("What's 2 + 2?")
+    assert response == "The result of 2 + 2 is 4."
+
+def test_case_insensitivity(chatbot_fixture):
+    chatbot_instance = chatbot_fixture
+    response = chatbot_instance.respond("i am not fine ")
+    assert response == "Sorry to hear that what are the possible ways we can help you ! feel free to ask any question"
+
+def test_unknown_command(chatbot_fixture):
+    chatbot_instance = chatbot_fixture
+    response = chatbot_instance.respond("/unknown")
+    assert response == "Unknown command. Type /help for available commands."
+
+
 def test_chatbot_response():
     query = "Hello, chatbot!"
     conversation_string = get_conversation_string()
@@ -31,6 +93,3 @@ def test_non_greeting_response():
     context = find_match(refined_query)
     response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
     assert response  # Add your assertion logic here
-
-# Add more test cases as needed
-
