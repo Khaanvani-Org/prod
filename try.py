0e5585e2f0993af1fa6cfba3f6ce1ad358af3b49
s-sukriti <eng21ct0039@dsu.edu.in>
2023-11-06 08:15:27 +0000

    Update from https://github.com/Khaanvani-Org/test/commit/96de7e7f20e6eebfebf06881794dfaf45a815186

--- try.py
+++ try.py
@@ -1,4 +1,10 @@
-num =1
-n2= 23
-sum=num+n2
-print("sum=",sum)
+# This program adds two numbers
+
+num1 = 33
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of the numbers {0} and {1} {2}'.format(num1, num2, sum))
