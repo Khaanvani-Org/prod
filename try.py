83d1b1048dd79f9939d183fa3b8c97b40a268dd1
s-sukriti <eng21ct0039@dsu.edu.in>
2024-01-18 17:18:30 +0000

    Update from https://github.com/Khaanvani-Org/test/commit/1d514d7a9ef51902f858b92c04fbf73b1ed13a77

--- try.py
+++ try.py
@@ -1,15 +1,10 @@
 # This program adds two numbers
 
 num1 = 23
-num2 = 11
+num2 = 12
 
 # Add two numbers
 sum = num1 + num2
 
 # Display the sum
 print('The sum of the numbers {0} and {1} {2}'.format(num1, num2, sum))
-#1st error
-#2nd error
-#3rd error.
-#4
-#5 
